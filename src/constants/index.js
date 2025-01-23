147a045cf2113015ed648555917202bcc11a980c
Sarah ElSherbiny <sarah.elsherbinyy@gmail.com>
2025-01-23 12:27:20 +0200

    added new experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -132,19 +132,19 @@ const experiences = [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  // {
-  //   title: "React Native Developer",
-  //   company_name: "Tesla",
-  //   icon: tesla,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2021 - Feb 2022",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+  {
+    title: "FrontEnd Engineer",
+    company_name: "4sale International",
+    icon: reactjs,
+    iconBg: "#E6DEDD",
+    date: "August 2024 - present",
+    points: [
+      "Developing new features and maintaining web applications using Next.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
   // {
   //   title: "Web Developer",
   //   company_name: "Shopify",
